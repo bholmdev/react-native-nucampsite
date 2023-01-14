951fcf00df28d02bef651d482ed76f84cd705546
Brandon Holman <bholmdev@gmail.com>
2023-01-14 14:34:20 -0600

    Week 1 Workshop

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -5,6 +5,8 @@ import DirectoryScreen from "./DirectoryScreen"
 import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "./HomeScreen";
+import AboutScreen from "./AboutScreen";
+import ContactScreen from "./ContactScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -19,9 +21,37 @@ const HomeNavigator = () => {
     return (
         <Stack.Navigator screenOptions={screenOptions}>
             <Stack.Screen
-                name="Home"
+                name="About"
                 component={HomeScreen}
                 options={{ title: "Home" }}
+            />
+        </Stack.Navigator>
+    );
+};
+
+const AboutNavigator = () => {
+    const Stack = createStackNavigator();
+
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name="Contact"
+                component={AboutScreen}
+                options={{ title: "About" }}
+            />
+        </Stack.Navigator>
+    );
+};
+
+const ContactNavigator = () => {
+    const Stack = createStackNavigator();
+
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name="Home"
+                component={ContactScreen}
+                options={{ title: "Contact Us" }}
             />
         </Stack.Navigator>
     );
@@ -74,6 +104,16 @@ const Main = () => {
                 name="Directory"
                 component={DirectoryNavigator}
                 options={{ title: "Directory" }}
+            />
+            <Drawer.Screen
+                name="About"
+                component={AboutNavigator}
+                options={{ title: "About" }}
+            />
+            <Drawer.Screen
+                name="Contact"
+                component={ContactNavigator}
+                options={{ title: "Contact Us" }}
             />
         </Drawer.Navigator>
     </View>
