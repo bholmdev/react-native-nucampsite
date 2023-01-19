a172a396d960ba5791e66f6b689cb04d32570434
Brandon Holman <bholmdev@gmail.com>
2023-01-19 14:46:02 -0600

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -3,12 +3,13 @@ import { Card, ListItem, Avatar } from "react-native-elements";
 import { ScrollView } from "react-native-gesture-handler";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/LoadingComponents";
 
 const Mission = () => {
-    return(
+    return (
         <Card>
             <Card.Title>Our Mission</Card.Title>
-            <Card.Divider/>
+            <Card.Divider />
             <Text style={{ margin: 10 }}>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
         </Card>
     );
@@ -16,6 +17,26 @@ const Mission = () => {
 
 const AboutScreen = () => {
     const partners = useSelector(state => state.partners);
+
+    if (partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Loading />
+                </Card>
+            </ScrollView>
+        )
+    }
+
+    if (partners.errMess) {
+        <ScrollView>
+            <Mission />
+            <Card>
+                <Text>{partners.errMess}</Text>
+            </Card>
+        </ScrollView>
+    }
 
     return (
         <ScrollView>
@@ -27,7 +48,7 @@ const AboutScreen = () => {
                     partners.partnersArray.map(partner => {
                         return (
                             <ListItem key={partner.id}>
-                                <Avatar 
+                                <Avatar
                                     rounded
                                     source={{ uri: baseUrl + partner.image }}
                                 />
@@ -35,7 +56,7 @@ const AboutScreen = () => {
                                     <ListItem.Title>{partner.name}</ListItem.Title>
                                     <ListItem.Subtitle>
                                         {partner.description}
-                                        </ListItem.Subtitle>
+                                    </ListItem.Subtitle>
                                 </ListItem.Content>
                             </ListItem>
                         );
